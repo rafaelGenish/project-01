61fc0438eec29c3230d4a95c676a1219e1e07f01
rafaelGenish <8483431@gmail.com>
2020-06-16 18:13:39 +0300

    first commit

--- index.js
+++ index.js
@@ -4,14 +4,40 @@ function Notes(text, date, time) {
     this.time = time;
 }
 
-function createNoteElement(text, date, time) {
+// function createNoteElement(text, date, time) {
+    
+    
+ 
+// }
+
+let list = [];
+
+let newNote = null;
+document.getElementById('add').addEventListener('click', () => {
+    const text = document.getElementById('text');
+    const date = document.getElementById('date');
+    const time = document.getElementById('time');
+    
+    if (text.value === '') {
+        text.focus()
+        return
+    }
+    // const theNote = document.createElement('div');
+    
+    const newNote = new Notes(text.value, date.value, time.value);
     const theNote = document.createElement('div');
     theNote.classList = 'note';
-    theNote.innerHTML = `<div class="text-note">Your note:<br>${text}</div>
-    <div class="date-note">date: ${date}</div>
-    <div class="time-note">time: ${time}</div>
+    theNote.innerHTML = `<div class="text-note">Your note:<br>${text.value}</div>
+    <div class="date-note">date: ${date.value}</div>
+    <div class="time-note">time: ${time.value}</div>
     <div id="dlt" class="delete  delete-fade-out"/>`
+    list.push(newNote);
+    localStorage.setItem('current', JSON.stringify(list))
     
+    // const theNote = createNoteElement(text.value, date.value, time.value)
+    document.getElementById('notes-zone').appendChild(theNote);
+    
+    text.focus()
     const deleteBtn = theNote.getElementsByClassName('delete')[0]
     
     theNote.addEventListener('mouseenter', (e) => {
@@ -23,55 +49,49 @@ function createNoteElement(text, date, time) {
         deleteBtn.classList.remove('delete-fade-in')
         deleteBtn.classList.add('delete-fade-out')
     })
-    
-    deleteBtn.addEventListener('click', () => {
-        list.splice(list.indexOf(theNote.text), 1)
-        console.log(list)
-        deleteBtn.parentElement.remove()    
+
+    deleteBtn.addEventListener('click' , () => {
+        deleteBtn.parentElement.remove()
+        list.splice(list.indexOf(newNote), 1);
         localStorage.setItem('current', JSON.stringify(list))
     })
+})
 
-    return theNote
-}
-let list = [];
-let newNote = null;
-document.getElementById('add').addEventListener('click', () => {
-    const text = document.getElementById('text');
-    const date = document.getElementById('date');
-    const time = document.getElementById('time');
+document.addEventListener('DOMContentLoaded', (e) => {
+    const loadedList = JSON.parse(localStorage.getItem('current'));
+    for (let i = 0; i < loadedList.length; i++) {
+        let note = loadedList[i];
+        list.push(note);
+ 
+        const loadNote = new Notes(note.text, note.date, note.time);
+        const theNote = document.createElement('div');
+    theNote.classList = 'note';
+    theNote.innerHTML = `<div class="text-note">Your note:<br>${loadNote.text}</div>
+    <div class="date-note">date: ${loadNote.date}</div>
+    <div class="time-note">time: ${loadNote.time}</div>
+    <div id="dlt" class="delete  delete-fade-out"/>`
+        document.getElementById('notes-zone').appendChild(theNote);
+        
+        const deleteBtn = theNote.getElementsByClassName('delete')[0]
 
-    if (text.value === '') {
-        text.focus()
-        return
+        theNote.addEventListener('mouseenter', (e) => {
+            deleteBtn.classList.remove('delete-fade-out')
+            deleteBtn.classList.add('delete-fade-in')
+        })
+        
+        theNote.addEventListener('mouseleave', (e) => {
+            deleteBtn.classList.remove('delete-fade-in')
+            deleteBtn.classList.add('delete-fade-out')
+        })
+    
+        deleteBtn.addEventListener('click' , () => {
+            deleteBtn.parentElement.remove()
+            list.splice(list.indexOf(note), 1);
+            localStorage.setItem('current', JSON.stringify(list))
+        })
     }
-   
-    const newNote = new Notes(text.value, date.value, time.value);
-    list.push(newNote);
-    localStorage.setItem('current', JSON.stringify(list))
-    
-    const theNote = createNoteElement(text.value, date.value, time.value)
-    document.getElementById('notes-zone').appendChild(theNote);
-    
-    text.focus()
-})
+ 
+    document.getElementById('text').focus()
+    return newNote
+ });
 
-document.getElementById('rmv').addEventListener('click', () => {
-    text.value = '';
-    date.value = '';
-    time.value = '';
-    text.focus()
-})
-
-document.addEventListener("DOMContentLoaded", function(event) {
-   const loadedList = JSON.parse(localStorage.getItem('current'));
-   for (let i = 0; i < loadedList.length; i++) {
-       const note = loadedList[i];
-       list.push(note);
-
-       const theNote = createNoteElement(note.text, note.date, note.time)
-       document.getElementById('notes-zone').appendChild(theNote);
-   }
-
-   document.getElementById('text').focus()
-   return newNote
-});
